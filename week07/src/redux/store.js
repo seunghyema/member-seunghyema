25dc1fb68080a234b7e64cf0cbb9a3b2cb28d40c
seunghyema <seunghyema53@gmail.com>
2026-05-26 19:18:59 +0900

    week08과제 제출 수정본
    week08 과제 2번째 제출

--- week07/src/redux/store.js
+++ week07/src/redux/store.js
@@ -1,29 +1,21 @@
-import { combineReducers, createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const TOGGLE_THEME = "theme/toggle";
-
-const initialThemeState = {
-  mode: "dark",
-};
-
-function themeReducer(state = initialThemeState, action) {
-  switch (action.type) {
-    case TOGGLE_THEME:
-      return {
-        ...state,
-        mode: state.mode === "dark" ? "light" : "dark",
-      };
-    default:
-      return state;
-  }
-}
-
-const rootReducer = combineReducers({
-  theme: themeReducer,
+const themeSlice = createSlice({
+  name: "theme",
+  initialState: {
+    mode: "dark",
+  },
+  reducers: {
+    toggleTheme: (state) => {
+      state.mode = state.mode === "dark" ? "light" : "dark";
+    },
+  },
 });
 
-export const store = createStore(rootReducer);
+export const { toggleTheme } = themeSlice.actions;
 
-export const toggleTheme = () => ({
-  type: TOGGLE_THEME,
+export const store = configureStore({
+  reducer: {
+    theme: themeSlice.reducer,
+  },
 });
